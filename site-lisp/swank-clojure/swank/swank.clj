fd37d800cbe742572abb09b1bf6cf197fc0e554a
Shawn Hoover <shawn@xia.com>
2008-09-08 11:04:25 -0700

    update swank-clojure to latest ns, in-ns usage

--- site-lisp/swank-clojure/swank/swank.clj
+++ site-lisp/swank-clojure/swank/swank.clj
@@ -1,12 +1,10 @@
-(clojure/create-ns 'swank)
-(clojure/ns swank
+(ns swank
   (:import (java.io InputStreamReader PushbackReader StringReader Reader
                     BufferedReader FileReader
                     OutputStreamWriter FileWriter Writer StringWriter
                     OutputStream PrintStream File)
            (clojure.lang LineNumberingPushbackReader)
            (java.net ServerSocket Socket InetAddress)
-           (java.util.zip ZipFile)))
-(clojure/refer 'clojure :exclude '(load-file))
-
-(load-resources "swank-clojure.clj" "swank-c-p-c.clj" "swank-arglists.clj")
+           (java.util.zip ZipFile))
+  (:refer-clojure :exclude [load-file])
+  (:load-resources "swank-clojure.clj" "swank-c-p-c.clj" "swank-arglists.clj"))
