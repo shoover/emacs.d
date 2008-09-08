4f3f1a3e2a17da7641d590004da165297067205b
Shawn Hoover <shawn@xia.com>
2008-09-07 22:34:44 -0700

    fix duplicate swank-clojure path

--- init.el
+++ init.el
@@ -19,7 +19,6 @@
   (add-path "emacs/color/color-theme-6.6.0") ;; my color preferences
   (add-path "emacs/site-lisp") ;; elisp stuff I find on the 'net
   (add-path "emacs/site-lisp/clojure")
-  (add-path "emacs/site-lisp/swank-clojure")
   (add-path "emacs/site-lisp/org-6.06b/lisp")
   (add-path "emacs/site-lisp/remember-2.0")
   (add-path "emacs/site-lisp/slime-cvs")
