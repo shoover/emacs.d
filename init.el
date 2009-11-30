7fc5c6436c009439996ca922132a92ee57c9d57c
Shawn Hoover <shawn@xia.com>
2009-11-30 17:35:10 -0500

    line killing goodness from emacs-fu blog

--- init.el
+++ init.el
@@ -165,6 +165,22 @@ scan-error if not."
 ;; Shift+(left|right|up|down) to get to a window quicker than with C-x o
 (windmove-default-keybindings)
 
+;; 
+(defadvice kill-ring-save (before slick-copy activate compile)
+  "When called interactively with no active region, copy a single
+line instead."
+  (interactive (if mark-active (list (region-beginning) (region-end)) (message
+  "Copied line") (list (line-beginning-position) (line-beginning-position
+  2)))))
+
+(defadvice kill-region (before slick-cut activate compile)
+  "When called interactively with no active region, kill a single
+line instead."
+  (interactive
+    (if mark-active (list (region-beginning) (region-end))
+      (list (line-beginning-position)
+        (line-beginning-position 2)))))
+
 ;; Buffer switching
 ;;(require 'iswitchb)
 ;;(iswitchb-mode 1)
