53f9e2ef6d90d68babd1d3d441791cbd1d124dae
Shawn Hoover <shawn.hoover@gmail.com>
2014-05-19 10:04:19 -0400

    message to confirm ahk reload

--- lisp/autohotkey-mode.el
+++ lisp/autohotkey-mode.el
@@ -78,6 +78,7 @@
       (error "Buffer '%s' is not visiting a file!" (buffer-name)))
   (if (buffer-modified-p)
       (save-buffer))
-  (start-process "AutoHotkey" nil autohotkey-exe "/restart" (buffer-file-name)))
+  (start-process "AutoHotkey" nil autohotkey-exe "/restart" (buffer-file-name))
+  (message "Reloaded script %s" (buffer-file-name)))
 
 (provide 'autohotkey-mode)
