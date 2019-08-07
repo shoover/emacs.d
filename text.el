59f4607daf1497405d15918f69c5dce57b8b2496
Shawn Hoover <shawn.hoover@gmail.com>
2019-08-07 09:34:58 -0700

    Fix substring arg.

--- text.el
+++ text.el
@@ -484,7 +484,7 @@ Uses async-shell-command if a prefix arg is given."
 (defun capitalize-first-char-x (&optional string)
   "Capitalize only the first character of the input STRING."
   (when (and string (> (length string) 0))
-    (let ((first-char (substring string nil 1))
+    (let ((first-char (substring string 0 1))
           (rest-str   (substring string 1)))
       (concat (capitalize first-char) rest-str))))
 
