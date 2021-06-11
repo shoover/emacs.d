3a0a5623a17ff5ecad67c9689e50ee55c8b1356b
Shawn Hoover <shoover@sunpower.com>
2021-06-11 11:10:44 -0400

    Remove custom-set-faces from init. It's covered by org-fancy.el.

--- init.el
+++ init.el
@@ -356,18 +356,3 @@ Errors may occur.  Save Options to overwrite the customization file. The origina
 	      (x-popup-dialog t (list msg '("OK" . nil) 'no-cancel) "Warning")
 	    (message msg)))))
 ;; End compatibility check
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(org-document-title ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.6 :underline nil))))
- '(org-level-1 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.45))))
- '(org-level-2 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4 :slant italic))))
- '(org-level-3 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4 :slant italic))))
- '(org-level-4 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4 :slant italic))))
- '(org-level-5 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4 :slant italic))))
- '(org-level-6 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4))))
- '(org-level-7 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4))))
- '(org-level-8 ((t (:inherit default :weight bold :foreground "#bdbc91" :underline nil :font "Calibri" :height 1.4)))))
