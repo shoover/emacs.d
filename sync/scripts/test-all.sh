da9e93bca4b7654fe3c0f0cd3018770ae93154d4
Shawn Hoover <shawn@shawnhoover.dev>
2026-02-12 07:17:38 -0500

    Add Swift and combined test runner scripts

---
+++ sync/scripts/test-all.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+sync/scripts/test-swift.sh
+sync/scripts/test-elisp.sh
---
+++ sync/scripts/test-swift.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env zsh
+set -euo pipefail
+
+cd sync/swift/orgrem
+swift test
